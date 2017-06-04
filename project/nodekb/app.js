3acf537ff837d932fdd8168ae20fd05b51fbb707
Damini Sinha <Damini Sinha>
2017-06-04 21:40:02 +0530

    node and express example commit

---
+++ project/nodekb/app.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const config = require('./config/database');
+
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
+db.once('open',function(){
+  console.log('Connected to MongoDB');
+});
+//Check for DB errors
+db.on('error',function(err){
+  console.log(err);
+});
+//Init app
+const app = express();
+//Bring in Models
+let Article = require('./models/article');
+//Load view engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+//Get static folder
+app.use(express.static(path.join(__dirname,'public')));
+//  Express Session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+}))
+
+//Express Messages middleware
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+// Passport Config
+require('./config/passport')(passport);
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+});
+
+//Home Route
+app.get('/',function(req,res){
+  Article.find({},function(err,articles){
+    if(err){
+      console.log(err)
+    }else{
+      res.render('index',{
+        title:'Articles',
+        articles:articles
+
+    });
+  }
+  });
+});
+
+//Route files
+let articles = require('./routes/article');
+let users = require('./routes/users');
+app.use('/articles',articles);
+app.use('/users',users);
+//Start server
+app.listen('3000',function(){
+  console.log('Server started on port 3000....');
+});
